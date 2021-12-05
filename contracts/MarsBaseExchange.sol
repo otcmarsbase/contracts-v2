346eef139e8b959a2dfb7891d843ec48f0c606c4
Sam Sheffres <samsheff@yandex.ru>
2021-12-05 12:42:16 +0700

    Add Override Functions

--- contracts/MarsBaseExchange.sol
+++ contracts/MarsBaseExchange.sol
@@ -9,6 +9,7 @@ contract MarsBaseExchange {
     using SafeMath for uint256;
 
     uint256 nextOfferId;
+
     struct MBOffer {
       IERC20 tokenIn;
       IERC20 tokenOut;
@@ -18,24 +19,53 @@ contract MarsBaseExchange {
       address payoutAddress;
     }
 
-    constructor() public {
-    }
-
     mapping (uint256 => MBOffer) public offers;
 
+    function createOffer(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut, address offerer, address payoutAddress) public payable returns (uint256) {
+      MBOffer memory offer = initOffer(tokenIn, tokenOut, amountIn, amountOut, offerer, payoutAddress);
+      offers[nextOfferId] = offer;
+
+      require(offer.tokenIn.transferFrom(msg.sender, address(this), amountIn));
+      
+      nextOfferId ++;
+
+      return nextOfferId;
+    }
+
     function createOffer(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut, address payoutAddress) public payable returns (uint256) {
-      MBOffer storage offer = offers[nextOfferId];
+      MBOffer memory offer = initOffer(tokenIn, tokenOut, amountIn, amountOut, msg.sender, payoutAddress);
+      offers[nextOfferId] = offer;
+
+      require(offer.tokenIn.transferFrom(msg.sender, address(this), amountIn));
+      
+      nextOfferId ++;
+
+      return nextOfferId;
+    }
+
+    function createOffer(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut) public payable returns (uint256) {
+
+      MBOffer memory offer = initOffer(tokenIn, tokenOut, amountIn, amountOut, msg.sender, msg.sender);
+      offers[nextOfferId] = offer;
+
+      require(offer.tokenIn.transferFrom(msg.sender, address(this), amountIn));
+      
+      nextOfferId ++;
+
+      return nextOfferId;
+    }
+
+    function initOffer(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut, address offerer, address payoutAddress) private pure returns (MBOffer memory) {
+      MBOffer memory offer;
       
       offer.tokenIn = IERC20(tokenIn);
       offer.tokenOut = IERC20(tokenOut);
       offer.amountIn = amountIn;
       offer.amountOut = amountOut;
-      offer.offerer = msg.sender;
+      offer.offerer = offerer;
       offer.payoutAddress = payoutAddress;
 
-      require(offer.tokenIn.transferFrom(msg.sender, address(this), amountIn));
-      nextOfferId ++;
-      return nextOfferId;
+      return offer;
     }
 
 }
