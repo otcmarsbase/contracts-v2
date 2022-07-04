ce38a6e7b3da65aee33e50961262a1b3431e0906
zlumer <zlumer@gmail.com>
2022-07-04 12:12:37 +0300

    tests: minimum size dynamic

--- test/missing/minimum-size.js
+++ test/missing/minimum-size.js
@@ -9,8 +9,223 @@ const { prepareEnvironment, ZERO, getOfferIdFromTx, getOfferDataFromTx } = requi
 
 describe("dynamic offers with minimum size", () =>
 {
-	it("should hold tokens before minimum size is covered")
-	it("should release tokens if minimum size is covered", async () =>
+	it("should hold tokens before minimum size is covered before cancel", async () =>
+	{
+		let env = await prepareEnvironment()
+		let { dex, mint, alice, bob, charlie, bat, usdt, parseLogs } = env
+
+		await approveMany(env, await mintAll(env, {
+			alice: {
+				usdt: "200",
+			},
+			bob: {
+				bat: "50",
+			},
+			charlie: {
+				bat: "100",
+			},
+		}))
+
+		async function doOfferBids(offer)
+		{
+			for (let i = 0; i < 20; i++)
+			{
+				let bidSuccess = await dex.connect(bob).acceptOffer(offer.id, bat.address, "1")
+				await bidSuccess.wait()
+			}
+			for (let i = 0; i < 20; i++)
+			{
+				let bidSuccess = await dex.connect(charlie).acceptOffer(offer.id, bat.address, "1")
+				await bidSuccess.wait()
+			}
+			return offer
+		}
+		let offer1 = await createOfferTokenToken(dex.connect(alice), usdt.address, "200", bat.address, "100", {
+			feeAlice: "0",
+			feeBob: "0",
+			minimumSize: "75",
+			cancelEnabled: true,
+		})
+		await expectBalances({ // offer created, bids not yet made
+			alice: {
+				usdt: "0",
+				bat: "0",
+			},
+			bob: {
+				usdt: "0",
+				bat: "50",
+			},
+			charlie: {
+				usdt: "0",
+				bat: "100",
+			},
+		})
+		await doOfferBids(offer1)
+		await expectBalances({ // bids made, but not yet sent
+			alice: {
+				usdt: "0",
+				bat: "0",
+			},
+			bob: {
+				usdt: "0",
+				bat: "30",
+			},
+			charlie: {
+				usdt: "0",
+				bat: "80",
+			},
+		})
+
+		await dex.connect(alice).cancelOffer(offer1.id)
+		await expectBalances({ // tokens returned to respective owners
+			alice: {
+				usdt: "200",
+				bat: "0",
+			},
+			bob: {
+				usdt: "0",
+				bat: "50",
+			},
+			charlie: {
+				usdt: "0",
+				bat: "100",
+			},
+		})
+	})
+	it("should hold tokens before minimum size is covered before buyout", async () =>
+	{
+		let env = await prepareEnvironment()
+		let { dex, mint, alice, bob, charlie, derek, bat, usdt, parseLogs } = env
+
+		await approveMany(env, await mintAll(env, {
+			alice: {
+				usdt: "200",
+			},
+			bob: {
+				bat: "50",
+			},
+			charlie: {
+				bat: "100",
+			},
+			derek: {
+				bat: "60",
+			}
+		}))
+
+		async function doOfferBids(offer)
+		{
+			for (let i = 0; i < 20; i++)
+			{
+				let bidSuccess = await dex.connect(bob).acceptOffer(offer.id, bat.address, "1")
+				await bidSuccess.wait()
+			}
+			for (let i = 0; i < 20; i++)
+			{
+				let bidSuccess = await dex.connect(charlie).acceptOffer(offer.id, bat.address, "1")
+				await bidSuccess.wait()
+			}
+			return offer
+		}
+		let offer1 = await createOfferTokenToken(dex.connect(alice), usdt.address, "200", bat.address, "100", {
+			feeAlice: "0",
+			feeBob: "0",
+			minimumSize: "75",
+			cancelEnabled: true,
+		})
+		await expectBalances({ // offer created, bids not yet made
+			alice: {
+				usdt: "0",
+				bat: "0",
+			},
+			bob: {
+				usdt: "0",
+				bat: "50",
+			},
+			charlie: {
+				usdt: "0",
+				bat: "100",
+			},
+		})
+		await doOfferBids(offer1)
+		await expectBalances({ // bids made, but not yet sent
+			alice: {
+				usdt: "0",
+				bat: "0",
+			},
+			bob: {
+				usdt: "0",
+				bat: "30",
+			},
+			charlie: {
+				usdt: "0",
+				bat: "80",
+			},
+		})
+
+		await dex.connect(derek).acceptOffer(offer1.id, bat.address, "60")
+		await expectBalances({ // bids made, but not yet sent
+			alice: {
+				usdt: "0",
+				bat: "100",
+			},
+			bob: {
+				usdt: "40",
+				bat: "30",
+			},
+			charlie: {
+				usdt: "40",
+				bat: "80",
+			},
+			derek: {
+				usdt: "120",
+				bat: "0",
+			},
+		})
+	})
+	it("should release tokens if minimum size is covered in first bid", async () =>
+	{
+		let env = await prepareEnvironment()
+		let { dex, mint, alice, bob, charlie, bat, usdt, parseLogs } = env
+
+		for (let [BAT_SENT, USDT_GOT] of [
+			["200", "100"],
+			["150", "75"],
+			["175", "87"],
+		])
+		{
+			await approveMany(env, await mintAll(env, {
+				alice: {
+					usdt: "100",
+				},
+				bob: {
+					bat: BAT_SENT,
+				}
+			}))
+
+			let offer = await createOfferTokenToken(dex.connect(alice), usdt.address, "100", bat.address, "200", {
+				feeAlice: "0",
+				feeBob: "0",
+				minimumSize: "75",
+			})
+	
+			// full buyout
+			await dex.connect(bob).acceptOffer(offer.id, bat.address, BAT_SENT)
+			await expectBalances(env, {
+				alice: {
+					usdt: "0",
+					bat: BAT_SENT,
+				},
+				bob: {
+					usdt: USDT_GOT,
+					bat: "0",
+				},
+			})
+			// burn tokens for next loop iteration
+			await bat.connect(alice).transfer(charlie.address, BAT_SENT)
+			await usdt.connect(bob).transfer(charlie.address, USDT_GOT)
+		}
+	})
+	it("should correctly release all held tokens after minimum size is covered", async () =>
 	{
 		let env = await prepareEnvironment()
 		let { dex, mint, alice, bob, charlie, bat, usdt, parseLogs } = env
@@ -82,7 +297,6 @@ describe("dynamic offers with minimum size", () =>
 			}
 		})
 	})
-	it("should correctly release all held tokens after minimum size is covered")
 })
 describe("static offers with minimum size", () =>
 {
