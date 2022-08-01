bff05c83d24fcf36a51dcb268d586238447351a4
zlumer <zlumer@gmail.com>
2022-08-01 15:29:59 +0300

    tests: approveMany target address override

--- test/token-utils.js
+++ test/token-utils.js
@@ -18,11 +18,11 @@ async function mintAll(env, balances)
 
 	return balances
 }
-async function approveMany(env, balances)
+async function approveMany(env, balances, dexAddress = env.dex.address)
 {
 	for (let name in balances)
 		for (let token in balances[name])
-			await env[token].connect(env[name]).approve(env.dex.address, balances[name][token])
+			await env[token].connect(env[name]).approve(dexAddress, balances[name][token])
 	
 	return balances
 }
