e385943c392267d6978e93b60b3bb57fcc008b53
zeeeeby <zeeeeby@users.noreply.github.com>
2023-04-19 17:37:20 +0000

    [auto] types updated

--- types/contracts/tokens/LOOKS.ts
+++ types/contracts/tokens/LOOKS.ts
@@ -2,104 +2,104 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import type BN from "bn.js"
-import type { ContractOptions } from "web3-eth-contract"
-import type { EventLog } from "web3-core"
-import type { EventEmitter } from "events"
+import type BN from "bn.js";
+import type { ContractOptions } from "web3-eth-contract";
+import type { EventLog } from "web3-core";
+import type { EventEmitter } from "events";
 import type {
-	Callback,
-	PayableTransactionObject,
-	NonPayableTransactionObject,
-	BlockType,
-	ContractEventLog,
-	BaseContract,
-} from "../../types"
+  Callback,
+  PayableTransactionObject,
+  NonPayableTransactionObject,
+  BlockType,
+  ContractEventLog,
+  BaseContract,
+} from "../../types";
 
 export interface EventOptions {
-	filter?: object
-	fromBlock?: BlockType
-	topics?: string[]
+  filter?: object;
+  fromBlock?: BlockType;
+  topics?: string[];
 }
 
 export type Approval = ContractEventLog<{
-	owner: string
-	spender: string
-	value: string
-	0: string
-	1: string
-	2: string
-}>
+  owner: string;
+  spender: string;
+  value: string;
+  0: string;
+  1: string;
+  2: string;
+}>;
 export type Transfer = ContractEventLog<{
-	from: string
-	to: string
-	value: string
-	0: string
-	1: string
-	2: string
-}>
+  from: string;
+  to: string;
+  value: string;
+  0: string;
+  1: string;
+  2: string;
+}>;
 
 export interface LOOKS extends BaseContract {
-	constructor(
-		jsonInterface: any[],
-		address?: string,
-		options?: ContractOptions
-	): LOOKS
-	clone(): LOOKS
-	methods: {
-		allowance(
-			owner: string,
-			spender: string
-		): NonPayableTransactionObject<string>
+  constructor(
+    jsonInterface: any[],
+    address?: string,
+    options?: ContractOptions
+  ): LOOKS;
+  clone(): LOOKS;
+  methods: {
+    allowance(
+      owner: string,
+      spender: string
+    ): NonPayableTransactionObject<string>;
 
-		approve(
-			spender: string,
-			amount: number | string | BN
-		): NonPayableTransactionObject<boolean>
+    approve(
+      spender: string,
+      amount: number | string | BN
+    ): NonPayableTransactionObject<boolean>;
 
-		balanceOf(account: string): NonPayableTransactionObject<string>
+    balanceOf(account: string): NonPayableTransactionObject<string>;
 
-		decimals(): NonPayableTransactionObject<string>
+    decimals(): NonPayableTransactionObject<string>;
 
-		decreaseAllowance(
-			spender: string,
-			subtractedValue: number | string | BN
-		): NonPayableTransactionObject<boolean>
+    decreaseAllowance(
+      spender: string,
+      subtractedValue: number | string | BN
+    ): NonPayableTransactionObject<boolean>;
 
-		increaseAllowance(
-			spender: string,
-			addedValue: number | string | BN
-		): NonPayableTransactionObject<boolean>
+    increaseAllowance(
+      spender: string,
+      addedValue: number | string | BN
+    ): NonPayableTransactionObject<boolean>;
 
-		name(): NonPayableTransactionObject<string>
+    name(): NonPayableTransactionObject<string>;
 
-		symbol(): NonPayableTransactionObject<string>
+    symbol(): NonPayableTransactionObject<string>;
 
-		totalSupply(): NonPayableTransactionObject<string>
+    totalSupply(): NonPayableTransactionObject<string>;
 
-		transfer(
-			to: string,
-			amount: number | string | BN
-		): NonPayableTransactionObject<boolean>
+    transfer(
+      to: string,
+      amount: number | string | BN
+    ): NonPayableTransactionObject<boolean>;
 
-		transferFrom(
-			from: string,
-			to: string,
-			amount: number | string | BN
-		): NonPayableTransactionObject<boolean>
-	}
-	events: {
-		Approval(cb?: Callback<Approval>): EventEmitter
-		Approval(options?: EventOptions, cb?: Callback<Approval>): EventEmitter
+    transferFrom(
+      from: string,
+      to: string,
+      amount: number | string | BN
+    ): NonPayableTransactionObject<boolean>;
+  };
+  events: {
+    Approval(cb?: Callback<Approval>): EventEmitter;
+    Approval(options?: EventOptions, cb?: Callback<Approval>): EventEmitter;
 
-		Transfer(cb?: Callback<Transfer>): EventEmitter
-		Transfer(options?: EventOptions, cb?: Callback<Transfer>): EventEmitter
+    Transfer(cb?: Callback<Transfer>): EventEmitter;
+    Transfer(options?: EventOptions, cb?: Callback<Transfer>): EventEmitter;
 
-		allEvents(options?: EventOptions, cb?: Callback<EventLog>): EventEmitter
-	}
+    allEvents(options?: EventOptions, cb?: Callback<EventLog>): EventEmitter;
+  };
 
-	once(event: "Approval", cb: Callback<Approval>): void
-	once(event: "Approval", options: EventOptions, cb: Callback<Approval>): void
+  once(event: "Approval", cb: Callback<Approval>): void;
+  once(event: "Approval", options: EventOptions, cb: Callback<Approval>): void;
 
-	once(event: "Transfer", cb: Callback<Transfer>): void
-	once(event: "Transfer", options: EventOptions, cb: Callback<Transfer>): void
+  once(event: "Transfer", cb: Callback<Transfer>): void;
+  once(event: "Transfer", options: EventOptions, cb: Callback<Transfer>): void;
 }
